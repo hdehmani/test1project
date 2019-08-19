5fce4851a02403f14e2d3bbd8b0ebf6a8794f4d1
Hassan Dehmani <hassan@dehmani.com>
2019-08-19 17:09:01 -0400

    My initial commit

---
+++ src/main/java/com/dehmani/docker/test1project/MyApp.java
@@ -0,0 +1,13 @@
+package com.dehmani.docker.test1project;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Hassan Dehmani!" );
+    }
+}
