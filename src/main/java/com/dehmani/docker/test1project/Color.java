6d508448e62355c0d850e148053e5c34272f83bb
Hassan Dehmani <hassan@dehmani.com>
2019-08-20 16:57:19 -0400

    Added a color number

--- src/main/java/com/dehmani/docker/test1project/Color.java
+++ src/main/java/com/dehmani/docker/test1project/Color.java
@@ -2,8 +2,16 @@ package com.dehmani.docker.test1project;
 
 public class Color {
 
-	
 	private String color;
+	private int number;
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
 
 	public String getColor() {
 		return color;
@@ -12,6 +20,5 @@ public class Color {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	
-	
+
 }
--- src/main/java/com/dehmani/docker/test1project/MyApp.java
+++ src/main/java/com/dehmani/docker/test1project/MyApp.java
@@ -12,12 +12,14 @@ public class MyApp {
 		customer.setLastname("Dehmani");
 		customer.setGender("male");
 		customer.setId(777);
-		
+
 		Color color = new Color();
-		
+
 		color.setColor("blue");
+		color.setNumber(111222);
 
 		System.out.println("Hello World from Customer " + customer.getFirstname() + " " + customer.getLastname()
-				+ " with id = " + customer.getId() + " and color = " + color.getColor() + "!");
+				+ " with id = " + customer.getId() + " and color = " + color.getColor() + " with color number = "
+				+ color.getNumber() + "!");
 	}
 }
