f87d210d0d9e0473efdb028c5344e2f8f1bd0bed
Hassan Dehmani <hassan@dehmani.com>
2019-08-20 16:04:15 -0400

    Changed name to Amir

--- src/main/java/com/dehmani/docker/test1project/MyApp.java
+++ src/main/java/com/dehmani/docker/test1project/MyApp.java
@@ -8,9 +8,9 @@ public class MyApp {
 	public static void main(String[] args) {
 
 		Customer customer = new Customer();
-		customer.setFirstname("Hassan");
+		customer.setFirstname("Amir");
 		customer.setLastname("Dehmani");
-		customer.setId(123);
+		customer.setId(999);
 
 		System.out.println("Hello World from Customer " + customer.getFirstname() + " " + customer.getLastname()
 				+ " with id = " + customer.getId() + "!");
