85c9be95de1ed1c49df571fca52974dc26fbb9a1
Hassan Dehmani <hassan@dehmani.com>
2019-08-20 15:36:39 -0400

    Added a Customer class

---
+++ src/main/java/com/dehmani/docker/test1project/Customer.java
@@ -0,0 +1,34 @@
+package com.dehmani.docker.test1project;
+
+public class Customer {
+
+	private String firstname;
+	private String lastname;
+	private int id;
+	
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+}
--- src/main/java/com/dehmani/docker/test1project/MyApp.java
+++ src/main/java/com/dehmani/docker/test1project/MyApp.java
@@ -4,10 +4,15 @@ package com.dehmani.docker.test1project;
  * Hello world!
  *
  */
-public class MyApp 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World from Hassan Dehmani 2019!" );
-    }
+public class MyApp {
+	public static void main(String[] args) {
+
+		Customer customer = new Customer();
+		customer.setFirstname("Hassan");
+		customer.setLastname("Dehmani");
+		customer.setId(123);
+
+		System.out.println("Hello World from Customer " + customer.getFirstname() + " " + customer.getLastname()
+				+ " with id = " + customer.getId() + "!");
+	}
 }
